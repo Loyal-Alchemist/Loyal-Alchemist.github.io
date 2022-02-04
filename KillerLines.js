eb60f395888411984598f8a0545b21fb234a19f4
Loyal-Alchemist <94395554+Loyal-Alchemist@users.noreply.github.com>
2022-02-04 10:09:46 -0700

    dsa

--- KillerLines.js
+++ KillerLines.js
@@ -1,12 +1,16 @@
 var messages = document.getElementById("messages");
 var messageArr = [["John Doe","","Hello Jane Doe"],["Jane Doe","","Hello there"]];
 var e1;
-fetch('KillerLinesServer.txt')
+var messageStore;
+var messageArrSingle;
+function updateMessages(){
+fetch('https://loyal-alchemist.github.io/KillerLinesServer.txt')
   .then(response => response.text())
   .then((data) => {
-    console.log(data)
+    messageStore = data;
   })
-function updateMessages(){
+messageArrSingle = messageStore.split("\n")[4].split(".").split(",");
+console.log(messageArrSingle);
 i = 0;
 messages.innerHTML = "";
 while(i<messageArr.length){
