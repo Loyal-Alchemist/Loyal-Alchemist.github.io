9db722115eb9705384cc3934c03a85fc2b592eb1
Loyal-Alchemist <94395554+Loyal-Alchemist@users.noreply.github.com>
2022-10-01 20:22:46 -0600

    das

--- TCR/Taxes.js
+++ TCR/Taxes.js
@@ -3,6 +3,8 @@ var curr1 = 1;
 var house = 0;
 var corruption = 0;
 var houseCost = 10;
+var lobbyist = 0;
+var lobbyistCost = 10;
 //}
 //{Game Tick
 function incrementer(){
@@ -13,17 +15,24 @@ setInterval(incrementer, 100);
 //}
 //{Calculate Currency
 function calcCurrency(){
-	curr1 += house/10;
+	curr1 = curr1 + (house/10) - (lobbyist/10);	
+	corruption += lobbyist/10;
 	document.getElementById("money").innerHTML = Math.trunc(curr1*10)/10;
 	document.getElementById("corruption").innerHTML = Math.round(corruption*10)/10;
 	document.getElementById("houses").innerHTML = house;
-	document.getElementById("moneyDir").innerHTML = house;
+	document.getElementById("moneyDir").innerHTML = (house) - (lobbyist);
 	document.getElementById("houseCost").innerHTML = "\n" + Math.trunc(houseCost*10)/10 + " Dollars";
+	document.getElementById("lobbyists").innerHTML = lobbyist;
+	document.getElementById("corruptionDir").innerHTML = lobbyist;
+	document.getElementById("lobbyCost").innerHTML = "\n" + Math.trunc(lobbyistCost*10)/10 + " Dollars";
+	
+	
 }
 //}
 //{Calculate Cost
 function calcCost(){
 	houseCost = Math.pow(10,(house-corruption+1));
+	lobbyistCost = Math.pow(10,(lobbyist-corruption+1));
 }
 //}
 //{Button Modifier
@@ -34,6 +43,13 @@ function actBuyHouseAttempt(){
 	if(curr1 >= houseCost){
 		curr1 -= houseCost;
 		house += 1;
+		corruption = 0;
+	}
+}
+function actBuyLobbyAttempt(){
+	if(curr1 >= lobbyistCost){
+		curr1 -= lobbyistCost;
+		lobbyist += 1;
 		corruption = 0;
 	}
 }
